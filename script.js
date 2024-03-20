18497f8e4b2a72198abd12ff89ea8fe249cdd5f3
Cleilson-Monteiro <cleilsonmonteiro680@gmail.com>
2024-03-20 10:35:24 -0300

    calculadora

---
+++ script.js
@@ -0,0 +1,20 @@
+function inserir(nun){
+    document.querySelector(".tela").innerHTML += nun;
+}
+function clean(){
+document.querySelector(".tela").innerHTML = "";
+}
+function back(){
+    let tela=document.querySelector(".tela").innerHTML;
+    document.querySelector(".tela").innerHTML = tela.substring(0,tela.length -1);
+}
+function calcular(){
+let tela=document.querySelector(".tela").innerHTML;
+if(tela){
+    document.querySelector(".tela").innerHTML = eval(tela)
+}
+else{
+    document.querySelector(".tela").innerHTML = "nenhum comando.";
+
+}
+}
